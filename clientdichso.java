359456ed0b21aa00378e1d825cdae709ce53e14d
XuanThien <92901587+xuanthien3003@users.noreply.github.com>
2022-04-04 19:41:47 +0700

    Add files via upload

---
+++ clientdichso.java
@@ -0,0 +1,78 @@
+package thien_61134397;
+
+import java.io.DataInputStream;
+
+import java.io.DataOutputStream;
+
+import java.io.IOException;
+
+import java.net.Socket;
+
+import java.util.Scanner;
+
+public class clientdichso {
+	public static void main(String[] args) throws IOException
+
+    {
+
+    // TODO code application logic here
+
+   
+
+        Socket mySocket = null;
+
+        DataOutputStream dos = null;
+
+        DataInputStream dis=null;
+
+       
+
+        try {
+
+            mySocket = new Socket("localhost", 1234);
+
+            dos = new DataOutputStream(mySocket.getOutputStream());
+
+            dis = new DataInputStream(mySocket.getInputStream());
+
+            Scanner input = new Scanner(System.in);
+
+            String s = null;
+
+            while(true)
+
+            {
+
+                System.out.print("\nNhap du lieu: ");
+
+                s =input.nextLine();
+
+                dos.writeUTF(s);
+
+               
+
+                String str=dis.readUTF();
+
+                System.out.print("Kết quả từ Server : "+str);
+
+            }
+
+        }
+
+        catch(Exception e) {
+
+            System.out.print("Ngat ket noi");
+
+            dis.close();
+
+            dos.close();
+
+            mySocket.close();
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
